8c32656852d74ec63574161749369a3dc3839c31
elias-gallo <eliasgallounla@gmail.com>
2026-06-23 21:15:02 -0300

    Main configurado para las pruebas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,40 @@
 package org.example;
 
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
-import java.io.IOException;
-
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.example.MiniLangLexer;
 import org.example.MiniLangParser;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String program = "programa.txt";
-
-        System.out.println("Interpreting file: " + program);
+        // Ponemos la ruta de tu archivo de prueba (ej: programa.txt)
+        String archivoPrueba = "programa.txt";
 
         try {
-            CharStream input = CharStreams.fromFileName(program);
-            MiniLangLexer lexer = new MiniLangLexer(input);
+            // parseo sintactico
+            MiniLangLexer lexer = new MiniLangLexer(CharStreams.fromFileName(archivoPrueba));
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             MiniLangParser parser = new MiniLangParser(tokens);
 
+            // arbol
             ParseTree tree = parser.programa();
 
-            System.out.println("Interpretation finished");
+            // analisis semántico
+            SymbolTable tablaSimbolos = new SymbolTable();
 
+            SemanticAnalyzer analizadoSemantico = new SemanticAnalyzer(tablaSimbolos);
+            analizadoSemantico.visit(tree);
+
+            // interprete
+            Interpreter interprete = new Interpreter(tablaSimbolos);
+            interprete.visit(tree);
+
+        } catch (RuntimeException e) {
+            System.err.println("\n[ERROR EN TIEMPO DE COMPILACIÓN/EJECUCIÓN]: " + e.getMessage());
         } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
+            System.err.println("\n[ERROR]: No se pudo leer el archivo " + archivoPrueba);
         }
     }
 }
